d42945068855e5ab8735c5ee14de42ed081c57dd
lukas0024 <104388560+lukas0024@users.noreply.github.com>
2023-03-20 07:31:26 +0100

    tailwind.config.js fix + background index + arrow events

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}",
-  "./dist/events/*.{html,js}"],
+  content: ["./*.{html,js}",
+  "./events/*.{html,js}"],
   theme: {
     extend: {},
   },
